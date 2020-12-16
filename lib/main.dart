7d1a0ecde7d3e529e631b643c9229d6c0c0f7ade
Mauro Cezar Johann <maurojohann@gmail.com>
2020-12-16 18:21:37 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'package:chat_flutter/utils/app_routes.dart';
+import 'screens/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      routes: {
+        AppRoutes.AUTH_HOME: (ctx) => HomePage(),
+      },
+    );
+  }
+}
